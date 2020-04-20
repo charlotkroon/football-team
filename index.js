166b861aa3bf2e0e1cb582540cc463f399c2fb1f
charliegabriella <charlotkroon@gmail.com>
2020-04-20 14:17:58 +0200

    Add Team Router to index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const app = express();
 
+//Routers
+const teamRouter = require("./team/router");
+
+//DB
 const db = require("./db");
 
+//Init
+const app = express();
 const port = 4000;
+
+//app.use
+app.use(teamRouter);
 
 app.listen(port, () => console.log(`Listening on :${port}`));
