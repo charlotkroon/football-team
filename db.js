b2298846e2f81766dab03aa8e231b6730295eeec
charliegabriella <charlotkroon@gmail.com>
2020-04-21 14:31:35 +0200

    Change db.sync from true to false

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 const databaseUrl = "postgres://postgres:constrasena@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl);
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => console.log("Database schema updated"))
   .catch(console.error);
 
