f412ad0d1e294619a13ada597b0d96689e5464cf
charliegabriella <charlotkroon@gmail.com>
2020-04-21 16:25:02 +0200

    Player Router: Add GET req with :id params

--- player/router.js
+++ player/router.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
 const Player = require("./model");
+const Team = require("../team/model");
 
 const router = new Router();
 
 router.get("/player", (req, res, next) => {
   Player.findAll()
+    .then((player) => {
+      res.send(player);
+    })
+    .catch(next);
+});
+
+router.get("/player/:id", (req, res, next) => {
+  Player.findByPk(req.params.id, { include: [Team] })
     .then((player) => {
       res.send(player);
     })
