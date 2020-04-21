1e8665799b16dc2e9163fb4ad09636fc4ea83c0e
charliegabriella <charlotkroon@gmail.com>
2020-04-21 15:47:00 +0200

    Add GET route

--- player/router.js
+++ player/router.js
@@ -2,3 +2,11 @@ const { Router } = require("express");
 const Player = require("./model");
 
 const router = new Router();
+
+router.get("/player", (req, res, next) => {
+  Player.findAll()
+    .then((player) => {
+      res.send(player);
+    })
+    .catch(next);
+});
